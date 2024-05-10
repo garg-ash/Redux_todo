a5784a8a9151bf1ae3150b49006700dd2f006b00
Ashish <ashishgarg0010@gmail.com>
2024-05-10 12:12:03 +0530

    add delete function

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <Todo />
-    
   </Provider>
 
 )
--- src/redux-Todo/slice.jsx
+++ src/redux-Todo/slice.jsx
@@ -8,16 +8,24 @@ export const slice = createSlice({
         tasks: [],
     },
     reducers: {
-        setInputValue: function(){},
-        handleSubmit : function(state, action){
+        setInputValue: function () { },
+        handleSubmit: function (state, action) {
             state.tasks = [...state.tasks, state.value];
+            state.value = ""
         },
-        handleChange: function(state, action){
+        handleChange: function (state, action) {
             state.value = action.payload;
+        },
+        handleDelete: function (state, action) {
+            state.tasks = state.tasks.filter((task, ind) => { return ind !== action.payload })
+        },
+        handleEdit: function (state, action){
+            const {index, newValue} = action.payload;
+            state.tasks[index] = newValue
         }
     }
 })
 
-export const {setInputValue, handleChange, handleSubmit} = slice.actions;
+export const { setInputValue, handleChange, handleSubmit, handleDelete, handleEdit } = slice.actions;
 const sliceReducer = slice.reducer;
 export default sliceReducer;
